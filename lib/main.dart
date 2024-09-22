0d657be3a4eb68ede4533c5eac102922e8a9e0ae
Udbhav <udbhav.k1234@gmail.com>
2024-09-22 19:25:06 +0530

    leetcode page added to routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:ripoff/pages/leetcode_page.dart';
 import 'package:ripoff/pages/login.dart';
 import 'package:ripoff/pages/loading.dart';
 import 'package:ripoff/pages/home.dart';
 
 void main() => runApp(MaterialApp(
+  // home: LeetcodePage(),
   initialRoute: '/',
   routes: {
     '/':(context) => const Login(),
     '/loading': (context) => const Loading(),
-    '/home': (context) => const Home()
+    '/home': (context) => const Home(),
+    '/leetcodePage':(context) => LeetcodePage(),
   },
 ));
