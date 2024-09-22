7f9a9da9e081fce46361d5c956bdd924325a0a23
Udbhav <udbhav.k1234@gmail.com>
2024-09-22 19:24:07 +0530

    leetcode page made

--- lib/pages/leetcode_page.dart
+++ lib/pages/leetcode_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ripoff/components/problem_count_card.dart';
 import 'package:ripoff/components/username_avatar.dart';
+import 'package:ripoff/components/radial_bargraph.dart';
 
 class LeetcodePage extends StatefulWidget {
   const LeetcodePage({super.key});
@@ -9,22 +11,38 @@ class LeetcodePage extends StatefulWidget {
 }
 
 class _LeetcodePageState extends State<LeetcodePage> {
+  Map d = {};
+  // void setUpLeetcodePage(){
+
+  // }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   Future.delayed(Duration.zero,(){
+  //     setUpLeetcodePage();
+  //   });
+  //
+  // }
   @override
   Widget build(BuildContext context) {
+    d = ModalRoute.of(context)?.settings.arguments as Map;
     return Scaffold(
-      backgroundColor:Color(0xFF161616),
+      backgroundColor: const Color(0xFF161616),
       body: Column(
         children: [
           SafeArea(
-            child: AvatarUsername(user: 'username',),
+            child: AvatarUsername(
+              user: d['username'],
+            ),
           ),
-          Container(
-            padding: EdgeInsets.fromLTRB(150, 100, 150, 100),
-            color: Color(0xFF0e0e0e),
-            child:Text("Problem Count",style: TextStyle(color: Colors.white),),
+          ProblemCountCard(
+            totalProblems: d['totalProblem'],
+            easyCount: d['totalEasyAccepted'],
+            mediumCount: d['totalMediumAccepted'],
+            hardCount: d['totalHardAccepted'],
           ),
         ],
       ),
-    );
+    );;
   }
 }
