e02b269af0d2bd064a3e2c5cc8a0ee221edbc5a6
Udbhav <udbhav.k1234@gmail.com>
2024-09-22 15:06:38 +0530

    class to store chart data

---
+++ lib/services/chart_data.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class ChartData{
+  final String x;
+  final double solved;
+  final Color color;
+  final double total;
+  ChartData(this.x,this.solved,this.total,this.color);
+}
