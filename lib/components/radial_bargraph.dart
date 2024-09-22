c126416d126ce64a2a27befe6880dfda61852595
Udbhav <udbhav.k1234@gmail.com>
2024-09-22 19:22:45 +0530

    widget to draw graph is made

--- lib/components/radial_bargraph.dart
+++ lib/components/radial_bargraph.dart
@@ -3,12 +3,32 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:ripoff/services/chart_data.dart';
 
 class RadialBarChart extends StatelessWidget {
+  final int easy;
+  final int medium;
+  final int hard;
+  String? extremeCountOneName;
+  String? extremeCountTwoName;
+  int? extremeCount1;
+  int? extremeCount2;
+
+  RadialBarChart(this.easy, this.medium, this.hard);
+
   @override
   Widget build(BuildContext context) {
     List<ChartData> chartData = [
-      ChartData("Hard",1, 826,Colors.red,),
-      ChartData('Medium', 44, 1726,Colors.orange,),
-      ChartData('Easy', 25, 826,Colors.green[300]!),
+      ChartData(
+        "Hard",
+        1,
+        826,
+        Colors.red,
+      ),
+      ChartData(
+        'Medium',
+        44,
+        1726,
+        Colors.orange,
+      ),
+      ChartData('Easy', 25, 826, Colors.green[300]!),
     ];
 
     return Container(
@@ -22,7 +42,9 @@ class RadialBarChart extends StatelessWidget {
             xValueMapper: (ChartData data, _) => data.x,
             yValueMapper: (ChartData data, _) {
               double percentage = (data.solved / data.total) * 100;
-              return (percentage < 5 && percentage > 0)? 5 : percentage; // Minimum 5% fill
+              return (percentage < 5 && percentage > 0)
+                  ? 5
+                  : percentage; // Minimum 5% fill
             },
             pointColorMapper: (ChartData data, _) => data.color,
             maximumValue: 100,
@@ -31,7 +53,7 @@ class RadialBarChart extends StatelessWidget {
             gap: '15%',
             trackBorderWidth: 5,
             cornerStyle: CornerStyle.bothCurve,
-            ),
+          ),
         ],
       ),
     );
