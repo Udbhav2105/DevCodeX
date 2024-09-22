a61c6f42f199570fcfcf0ae8263cc74ac80bc274
Udbhav <udbhav.k1234@gmail.com>
2024-09-22 12:21:08 +0530

    leetcode page added

--- lib/pages/leetcode_page.dart
+++ lib/pages/leetcode_page.dart
@@ -11,13 +11,34 @@ class _LeetcodePageState extends State<LeetcodePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Color(0xFF333333),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(100),
-          color: Color(0xFF161616),
-          child:Text("Hellow how are\nyou doing"),
-        ),
+      backgroundColor:Color(0xFF161616),
+      body: Column(
+        children: [
+          SafeArea(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(30, 10, 0, 20),
+              child: Row(
+                children: [
+                  CircleAvatar(),
+                  SizedBox(width:30,),
+                  Text(
+                    'Username'.toUpperCase(),
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(150, 100, 150, 100),
+            color: Color(0xFF0e0e0e),
+            child:Text("Problem Count",style: TextStyle(color: Colors.white),),
+          ),
+        ],
       ),
     );
   }
