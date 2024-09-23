d0e7b7d78bb8761f689464a3adad43fbc6e81818
Udbhav <udbhav.k1234@gmail.com>
2024-09-23 09:41:12 +0530

    new chioce page added

---
+++ lib/pages/choice.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Choice extends StatefulWidget {
+  const Choice({super.key});
+
+  @override
+  State<Choice> createState() => _ChoiceState();
+}
+
+class _ChoiceState extends State<Choice> {
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> choices = [];
+    if
+    return Scaffold(
+      backgroundColor: Color(0xFF161616),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
