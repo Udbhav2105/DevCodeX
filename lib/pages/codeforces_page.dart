454fc071db6a014537a3bfa9e890c97634ca9541
Va1bhav512 <133973438+Va1bhav512@users.noreply.github.com>
2024-09-28 16:32:16 +0530

    Added chart and card using problem count card

--- lib/pages/codeforces_page.dart
+++ lib/pages/codeforces_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ripoff/components/doughnut_chart.dart';
+import 'package:ripoff/components/problem_count_card.dart';
 import 'package:ripoff/components/username_avatar.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:ripoff/services/chart_data_cf.dart';
+// import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Codeforces extends StatefulWidget {
   const Codeforces({super.key});
@@ -11,13 +14,13 @@ class Codeforces extends StatefulWidget {
 
 class CodeforcesState extends State<Codeforces> {
   Map d = {};
-  late TooltipBehavior _tooltip;
-  late List<ChartData> chartData;
+  // late TooltipBehavior _tooltip;
+  late List<ChartDataCF> chartDataCF;
 
   @override
   void initState() {
     super.initState();
-    _tooltip = TooltipBehavior(enable: true);
+    // _tooltip = TooltipBehavior(enable: true);
   }
 
   @override
@@ -25,101 +28,112 @@ class CodeforcesState extends State<Codeforces> {
     d = ModalRoute.of(context)?.settings.arguments as Map;
 
     // Prepare chart data
-    chartData = [
-      // ChartData('Easy (800-1300)', d['cfEasy'] ?? 0),
-      // ChartData('Medium (1301-1900)', d['cfMedium'] ?? 0),
-      // ChartData('Hard (1901-2600)', d['cfHard'] ?? 0),
-      // ChartData('Extreme (2601-3500)', d['cfExtreme'] ?? 0),
-      // ChartData('Unrated', d['cfUnrated'] ?? 0),
-      ChartData('Easy (800-1300)', d['cfData'].easySolved ?? 0),
-      ChartData('Medium (1301-1900)', d['cfData'].mediumSolved?? 0),
-      ChartData('Hard (1901-2600)', d['cfData'].hardSolved ?? 0),
-      ChartData('Extreme (2601-3500)', d['cfData'].extremeSolved?? 0),
-      ChartData('Unrated', d['cfData'].unratedSolved ?? 0),
+    chartDataCF = [
+      ChartDataCF('Easy (800-1300)', d['cfData'].easySolved ?? 0),
+      ChartDataCF('Medium (1301-1900)', d['cfData'].mediumSolved?? 0),
+      ChartDataCF('Hard (1901-2600)', d['cfData'].hardSolved ?? 0),
+      ChartDataCF('Extreme (2601-3500)', d['cfData'].extremeSolved?? 0),
+      ChartDataCF('Unrated', d['cfData'].unratedSolved ?? 0),
     ];
 
+    // return Scaffold(
+    //   backgroundColor: Colors.black, // Dark background
+    //   body: Padding(
+    //     padding: const EdgeInsets.all(20.0),
+    //     child: Column(
+    //       children: [
+    //         SafeArea(
+    //           child: AvatarUsername(
+    //             user: d['cfData'].username ?? 'User',
+    //             avatar: d['cfData'].avatar ?? 'default_avatar_url', // Replace with your default avatar URL
+    //           ),
+    //         ),
+    //         const SizedBox(height: 20),
+    //         Text(
+    //           '${d['cfData'].username ?? 'Unknown User'}',
+    //           style: const TextStyle(
+    //             color: Color(0xFFD4AF37), // Gold color
+    //             fontSize: 24,
+    //             fontWeight: FontWeight.bold,
+    //           ),
+    //         ),
+    //         Text(
+    //           'Rating: ${d['cfData'].userRating ?? 'N/A'}',
+    //           style: const TextStyle(
+    //             color: Colors.white,
+    //             fontSize: 18,
+    //           ),
+    //         ),
+    //         Text(
+    //           'Max Rating: ${d['cfData'].maxRating ?? 'N/A'}',
+    //           style: const TextStyle(
+    //             color: Colors.white,
+    //             fontSize: 18,
+    //           ),
+    //         ),
+    //         Text(
+    //           'Rank: ${d['cfData'].rank ?? 'N/A'}',
+    //           style: const TextStyle(
+    //             color: Colors.white,
+    //             fontSize: 18,
+    //           ),
+    //         ),
+    //         Text(
+    //           'Max Rank: ${d['cfData'].maxRank ?? 'N/A'}',
+    //           style: const TextStyle(
+    //             color: Colors.white,
+    //             fontSize: 18,
+    //           ),
+    //         ),
+    //         const SizedBox(height: 20),
+    //         Expanded(
+    //           child: Card(
+    //             color: const Color(0xFF1A1A1A), // Darker card background
+    //             shape: RoundedRectangleBorder(
+    //               borderRadius: BorderRadius.circular(16),
+    //             ),
+    //             elevation: 10,
+    //             child: SfCircularChart(
+    //               tooltipBehavior: _tooltip,
+    //               series: <CircularSeries<ChartData, String>>[
+    //                 DoughnutSeries<ChartData, String>(
+    //                   dataSource: chartData,
+    //                   xValueMapper: (ChartData data, _) => data.x,
+    //                   yValueMapper: (ChartData data, _) => data.y,
+    //                   dataLabelSettings: const DataLabelSettings(isVisible: true),
+    //                   radius: '80%', // Control the size of the chart
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
     return Scaffold(
-      backgroundColor: Colors.black, // Dark background
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
+      backgroundColor: const Color(0xFF161616),
+      body: Column(
           children: [
             SafeArea(
               child: AvatarUsername(
-                user: d['cfData'].username ?? 'User',
-                avatar: d['cfData'].avatar ?? 'default_avatar_url', // Replace with your default avatar URL
+                user: d['cfData'].username,
+                avatar: d['cfData'].avatar,
               ),
             ),
-            const SizedBox(height: 20),
-            Text(
-              '${d['cfData'].username ?? 'Unknown User'}',
-              style: const TextStyle(
-                color: Color(0xFFD4AF37), // Gold color
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'Rating: ${d['cfData'].userRating ?? 'N/A'}',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            Text(
-              'Max Rating: ${d['cfData'].maxRating ?? 'N/A'}',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            Text(
-              'Rank: ${d['cfData'].rank ?? 'N/A'}',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            Text(
-              'Max Rank: ${d['cfData'].maxRank ?? 'N/A'}',
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: Card(
-                color: const Color(0xFF1A1A1A), // Darker card background
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                elevation: 10,
-                child: SfCircularChart(
-                  tooltipBehavior: _tooltip,
-                  series: <CircularSeries<ChartData, String>>[
-                    DoughnutSeries<ChartData, String>(
-                      dataSource: chartData,
-                      xValueMapper: (ChartData data, _) => data.x,
-                      yValueMapper: (ChartData data, _) => data.y,
-                      dataLabelSettings: const DataLabelSettings(isVisible: true),
-                      radius: '80%', // Control the size of the chart
-                    ),
-                  ],
-                ),
-              ),
+            ProblemCountCard(
+              chart: DoughnutChart(chartDataCF),
+              totalProblems: d['cfData'].uniqueOkProblems.length,
+              easyCount: d['cfData'].easySolved ?? 0,
+              mediumCount: d['cfData'].mediumSolved ?? 0,
+              hardCount: d['cfData'].hardSolved ?? 0,
+              extremeCount1: d['cfData'].extremeSolved,
+              extremeCountOneName: "Extreme",
+              extremeCount2: d['cfData'].unratedSolved ?? 0,
+              extremeCountTwoName: "Unrated",
             ),
           ],
         ),
-      ),
     );
   }
-}
-
-// Model class for chart data
-class ChartData {
-  ChartData(this.x, this.y);
-
-  final String x;
-  final int y;
 }
