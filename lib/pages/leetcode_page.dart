c33d005946ad3a2c8c9aefd62567c4265f02e949
Udbhav <udbhav.k1234@gmail.com>
2024-09-28 18:48:24 +0530

    accessing contest data from LC api

--- lib/pages/leetcode_page.dart
+++ lib/pages/leetcode_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ripoff/components/contest_data.dart';
 import 'package:ripoff/components/lc_badges_card.dart';
 import 'package:ripoff/components/problem_count_card.dart';
 import 'package:ripoff/components/username_avatar.dart';
@@ -29,24 +30,27 @@ class _LeetcodePageState extends State<LeetcodePage> {
     d = ModalRoute.of(context)?.settings.arguments as Map<String,dynamic>;
     return Scaffold(
       backgroundColor: const Color(0xFF161616),
-      body: Column(
-          children: [
-            SafeArea(
-              child: AvatarUsername(
-                user: d['lcData'].lcUsername,
-                avatar: d['lcData'].lcAvatar,
+      body: SingleChildScrollView(
+        child: Column(
+            children: [
+              SafeArea(
+                child: AvatarUsername(
+                  user: d['lcData'].lcUsername,
+                  avatar: d['lcData'].lcAvatar,
+                ),
               ),
-            ),
-            ProblemCountCard(
-              chart: RadialBarChart(d['lcData'].totalAcEasy, d['lcData'].totalAcMedium,d['lcData'].totalAcHard),
-              totalProblems: d['lcData'].totalProblemCount,
-              easyCount: d['lcData'].totalAcEasy,
-              mediumCount: d['lcData'].totalAcMedium,
-              hardCount: d['lcData'].totalAcHard,
-            ),
-            BadgesCard(d['lcData'].badgeUrls),
-          ],
-        ),
+              ProblemCountCard(
+                chart: RadialBarChart(d['lcData'].totalAcEasy, d['lcData'].totalAcMedium,d['lcData'].totalAcHard),
+                totalProblems: d['lcData'].totalProblemCount,
+                easyCount: d['lcData'].totalAcEasy,
+                mediumCount: d['lcData'].totalAcMedium,
+                hardCount: d['lcData'].totalAcHard,
+              ),
+              BadgesCard(d['lcData'].badgeUrls),
+              ContestCard(contestData:d['lcData'].lcContest),
+            ],
+          ),
+      ),
       );
   }
 }
