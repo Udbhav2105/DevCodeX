797056573cb24859e27bf50be132eda4d70e55b4
Udbhav <udbhav.k1234@gmail.com>
2024-10-19 18:10:56 +0530

    sign in page UI created

--- lib/firebase_login.dart
+++ lib/firebase_login.dart
@@ -1,75 +1,125 @@
 import 'package:DevCodeX/services/app_color.dart';
 import 'package:flutter/material.dart';
 import 'package:DevCodeX/auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FirebaseLogin extends StatelessWidget {
   final AuthService _auth = AuthService();
+
   FirebaseLogin({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        centerTitle: true,
-        backgroundColor: AppColors.backgroundColor,
-        iconTheme: const IconThemeData(color: Colors.white),
-      ),
-      backgroundColor: AppColors.backgroundColor,
-      body: Center(
+      backgroundColor: Colors.black,
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton.icon(
-              onPressed: () async {
-                await _auth.signInAnonymously();
-              },
-              icon: const Icon(
-                Icons.arrow_forward,
-                color: AppColors.backgroundColor,
-              ),
-              label: const Text(
-                "Continue without signing in",
-                style: TextStyle(
-                  color: AppColors.backgroundColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
+          children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                height: 150,
+                color: Colors.black12,
+                child: const Text(
+                  "DevCodeX",
+                  style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.secondaryColor,
-                  padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  )),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton.icon(
-              onPressed: () async {
-                await _auth.signInWithGoogle();
-              },
-              icon: const Icon(
-                Icons.arrow_forward,
-                color: AppColors.backgroundColor,
-              ),
-              label: const Text(
-                "Sign in with Google",
-                style: TextStyle(
-                  color: AppColors.backgroundColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
+            // Adding a SizedBox to separate the title from the bottom container
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: AppColors.backgroundColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50),
+                    )),
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: Center(
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 40, 0, 40),
+                        child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 1.2,
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 100),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.78,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await _auth.signInAnonymously();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            minimumSize: Size(200, 60),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Continue without Signing In",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    letterSpacing: 0.7,
+                                    fontSize: 17),
+                              ),
+                              Icon(Icons.account_circle),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.78,
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await _auth.signInWithGoogle();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            minimumSize: Size(200, 60),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Sign In with Google",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    letterSpacing: 0.7,
+                                    fontSize: 17),
+                              ),
+                              FaIcon(FontAwesomeIcons.google),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.secondaryColor,
-                  padding: const EdgeInsets.fromLTRB(30, 15, 30, 15),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  )),
             ),
           ],
         ),
