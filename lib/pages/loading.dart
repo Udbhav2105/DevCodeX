45062ccf61a2b44e5b24cedaedd8a562a61b34bd
Va1bhav512 <133973438+Va1bhav512@users.noreply.github.com>
2024-10-25 15:01:21 +0530

    Fixed to get data from firestore or from previous page accordingly and added AuthService from provider

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -8,6 +8,7 @@ import 'package:DevCodeX/services/cfdata.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:DevCodeX/services/dcx_user.dart';
 import 'package:DevCodeX/auth.dart';
+import 'package:provider/provider.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -18,63 +19,94 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
   final FirebaseFirestore db = FirebaseFirestore.instance;
-  final AuthService _auth = AuthService();
+  late AuthService _auth;
   Map data = {};
-  // Function to fetch Codeforces data using the provided username
-  void setUpLeetcodeAndCodeforces() async {
-    // Fetch the passed data from previous screen (Leetcode and Codeforces usernames)
-    data = ModalRoute.of(context)?.settings.arguments as Map;
-    String lcUsername = data['lcUsername'];
-    String cfUsername = data['cfUsername']; // Get the Codeforces username
-    String gfgUsername = data['gfgUsername'];
 
-    // Save the data to Firestore
-    if (_auth.currentUser == null || _auth.currentUser?.uid == null) {
-        print('User is null');
-      } else {
-          final dcxUser = DcxUser(
-          cfUsername: data['cfUsername'],
-          lcUsername: data['lcUsername'],
-          gfgUsername: data['gfgUsername'],
-          uid: _auth.currentUser!.uid);
-          final docRef = db.collection('users').withConverter(
+  void setUpLeetcodeAndCodeforces(
+      String lcUsername, String cfUsername, String gfgUsername) async {
+    if (_auth.currentUser == null) return;
+
+    final uid = _auth.currentUser!.uid;
+    final dRR = db.collection('users').doc(uid);
+
+    final userDoc = await dRR.get();
+    if (!userDoc.exists) {
+      final dcxUser = DcxUser(
+        cfUsername: cfUsername,
+        lcUsername: lcUsername,
+        gfgUsername: gfgUsername,
+        uid: uid,
+      );
+    final docRef = db.collection('users').withConverter(
             fromFirestore: DcxUser.fromFirestore,
             toFirestore: (dcxUser, options) => dcxUser.toFirestore(),
-          ).doc(_auth.currentUser!.uid);
+          ).doc(uid);
           await docRef.set(dcxUser);
-      }
-    // Fetch Codeforces data
-    // Map<String, int> codeforcesData = await fetchCodeforcesData(cfUsername);
+    }
+
     CfData codeforcesData = CfData(username: cfUsername);
     await codeforcesData.fetchUserStatus();
     await codeforcesData.fetchUserInfo();
 
-
-    // Fetch GFG data
     GfgData gfgData = GfgData(userName: gfgUsername);
     await gfgData.authenticate();
 
-    // Fetch Leetcode data
     Lc instance = Lc(lcUsername: lcUsername);
+    await instance.getEverything();
 
-      await instance.getEverything();
-    print('Lc Auth: ${instance.lcAuth}\nCf Auth ${codeforcesData.cfAuth}\nGfg auth ${gfgData.gfgAuth}');
+    print('Lc Auth: ${instance.lcAuth}\nCf Auth ${codeforcesData.cfAuth}\nGfg Auth ${gfgData.gfgAuth}');
 
 
-    Navigator.pushReplacementNamed(context, '/home', arguments: {
-      'cfData': codeforcesData,
-      'lcData': instance,
-      'gfgData': gfgData,
-    });
+    if (mounted) {
+      Navigator.pushReplacementNamed(context, '/home', arguments: {
+        'cfData': codeforcesData,
+        'lcData': instance,
+        'gfgData': gfgData,
+      });
+    }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero, () {
-      setUpLeetcodeAndCodeforces();
-    });
-  }
+@override
+void didChangeDependencies() {
+  super.didChangeDependencies();
+  _auth = Provider.of<AuthService>(context);
+}
+@override
+void initState() {
+  super.initState();
+
+  WidgetsBinding.instance.addPostFrameCallback((_) async {
+    final arguments = ModalRoute.of(context)?.settings.arguments as Map<String, String>?;
+
+    if (arguments != null) {
+      // Use arguments passed to this page
+      data = arguments;
+      setUpLeetcodeAndCodeforces(
+          data['lcUsername']!, data['cfUsername']!, data['gfgUsername']!);
+    } else if (_auth.currentUser != null) {
+      // No arguments, check Firestore for existing data
+      final docRef = db.collection('users').doc(_auth.currentUser!.uid);
+      final userDoc = await docRef.get();
+
+      if (userDoc.exists) {
+        final userData = userDoc.data() as Map<String, dynamic>;
+        data = {
+          'lcUsername': userData['lcUsername'],
+          'cfUsername': userData['cfUsername'],
+          'gfgUsername': userData['gfgUsername'],
+        };
+        setUpLeetcodeAndCodeforces(
+            data['lcUsername'], data['cfUsername'], data['gfgUsername']);
+      } else {
+        // If no data exists in Firestore, navigate to login page
+        if (mounted) {
+          Navigator.pushReplacementNamed(context, '/login');
+        }
+      }
+    }
+  });
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -89,4 +121,3 @@ class _LoadingState extends State<Loading> {
     );
   }
 }
-
