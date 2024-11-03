6c4c4b0d0da702d6e823efe385feda6872c90ecd
Va1bhav512 <133973438+Va1bhav512@users.noreply.github.com>
2024-11-03 20:58:19 +0530

    Removed self from search

--- lib/services/search_firebase_users.dart
+++ lib/services/search_firebase_users.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:DevCodeX/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 
-Future<List<DocumentSnapshot>> searchFirebaseUsers(String query) async {
+Future<List<DocumentSnapshot>> searchFirebaseUsers(BuildContext context, String query) async {
+  final _auth = Provider.of<AuthService>(context, listen: false);
+  final currentUserId = _auth.currentUser?.uid;
+
   final results = await FirebaseFirestore.instance
       .collection('users')
+      .where('uid', isNotEqualTo: currentUserId)
       .where('uid', isEqualTo: query)
       .get();
 
